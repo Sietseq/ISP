34e03f8b2baff3cc629a254a5a9fe9a2da032d71
Sietse de Backer <59657884+Sietseq@users.noreply.github.com>
2024-07-24 17:39:37 -0300

    Update job.py

--- Siku/job.py
+++ Siku/job.py
@@ -1,17 +1,19 @@
 import requests
 import patoolib
 import os
-
+import time
 count = 0
-os.mkdir("./upload")
+#os.mkdir("./upload")
 while(True):
     if_ready = requests.get('http://52.14.130.151/getready')
-    if (bool(if_ready)):
-        os.mkdir("./"+ str(count)) 
-        os.mkdir("./"+ str(count) +"/current")
-        os.mkdir("./"+ str(count) +"/extraction")
-        patoolib.extract_archive("upload/file.tar", "./"+ str(count) +"/extraction")
-        os.remove("upload/file.tar")
+    if (if_ready.text == "ready"):
+        if not os.path.exists("./" + str(count)):
+            os.mkdir("./"+ str(count))
+            os.mkdir("./"+ str(count) +"/current")
+            os.mkdir("./"+ str(count) +"/extraction")
+        patoolib.extract_archive("upload/file.zip", outdir="./"+ str(count) +"/extraction", interactive=False)
+        os.remove("upload/file.zip")
         requests.get("http://52.14.130.151/removequeue")
-        os.popen("sbatch -D `pwd` --export=var_name='" + str(count) + "'job.sh")
-
+        txt = os.system("bash job.sh " + str(count))
+        count += 1
+    time.sleep(0.5)
