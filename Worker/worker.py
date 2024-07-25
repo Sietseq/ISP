930b1154dc525c7e57c53d97dff812cc5a9dd8b8
Sietse de Backer <59657884+Sietseq@users.noreply.github.com>
2024-07-24 23:19:39 -0300

    Update worker.py

--- Worker/worker.py
+++ Worker/worker.py
@@ -7,7 +7,7 @@ import ast
 
 import subprocess
 
-
+# Used to visualize the progress in downloading
 pbar = None
 def show_progress(block_num, block_size, total_size):
     global pbar
@@ -23,24 +23,32 @@ def show_progress(block_num, block_size, total_size):
         pbar = None
 
 def main():
-    if not os.path.exists('download/'):
-        os.mkdir('download/')
+    # Open the file that has record of links to be downloaded. 
     with open('files.txt') as read_file:
         for line in read_file:
+            # Make easier name
             base_name = line.split('/')[-1].split('.')[0]
+            
+            # Start downloading
             print('Downloading: ' + line)
             urllib.request.urlretrieve(line, 'download/archive.zip' , show_progress)
 
+            # Add to queue
             print('Uploading')
             requests.get('http://52.14.130.151/addqueue?file=' + base_name)
             ready = False
+
+            # Keep checking if it is possible to upload
             while(not ready):
+                # If the file is first in queue start uploading
                 my_list = requests.get('http://ip/checkqueue')
                 my_list = ast.literal_eval(my_list.text)
                 if (my_list[1] == base_name):
                     print(base_name)
                     ready = True
+                    # Upload file
                     text = os.system("sshpass -p 'pwd' scp ./download/archive.zip" + ' usr@siku.ace-net.ca:~/ISP/Siku/upload/file.zip ')
+                    # Notify that upload is done
                     requests.get('http://52.14.130.151/setready')
                 time.sleep(0.5)
             os.remove('download/archive.zip')
